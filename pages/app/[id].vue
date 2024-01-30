bb4c23f7e5e8e6016fac9551294983315c58426b
jolthegreat <katoshin0805@gmail.com>
2024-01-30 16:49:25 +0900

    Meta tags

--- pages/app/[id].vue
+++ pages/app/[id].vue
@@ -95,20 +95,20 @@ export default {
 <template>
   <Head>
     <Title>{{ title }}</Title>
-    <Meta name="description" content="{{ catchphrase }}"/>
-    <Meta property="og:title" content="{{ title }}"/>
-    <Meta property="og:description" content="{{ catchphrase }}"/>
-    <Meta property="og:image" content="{{ images[0] }}"/>
-    <Meta property="og:url" content="{{url}}"/>
+    <Meta name="description" v-bind:content="catchphrase"/>
+    <Meta property="og:title" v-bind:content="title"/>
+    <Meta property="og:description" v-bind:content="catchphrase"/>
+    <Meta property="og:image" v-bind:content="images[0]"/>
+    <Meta property="og:url" v-bind:content="url"/>
     <Meta property="og:type" content="website"/>
     <Meta property="og:site_name" content="Circle"/>
-    <Meta property="og:locale" content="ja_JP"/>
     <Meta name="twitter:card" content="summary_large_image"/>
     <Meta name="twitter:site" content="@Circle"/>
-    <Meta name="twitter:url" content="{{url}}"/>
-    <Meta name="twitter:title" content="{{ title }}"/>
-    <Meta name="twitter:description" content="{{ catchphrase }}"/>
-    <Meta name="twitter:image" content="{{ images[0] }}"/>
+    <Meta name="twitter:title" v-bind:content="title"/>
+    <Meta name="twitter:description" v-bind:content="catchphrase"/>
+    <Meta name="twitter:image" v-bind:content="images[0]"/>
+    <Meta name="twitter:url" v-bind:content="url"/>
+    <Meta name="twitter:domain" content="Circle"/>
   </Head>
 
   <div id="article">
