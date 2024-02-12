69204391ff84d07ad1b3a24261862c00527b0e2b
jolthegreat <katoshin0805@gmail.com>
2024-02-12 16:10:57 +0900

    Google Analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -46,7 +46,8 @@ export default defineNuxtConfig({
                 projectId: process.env.PROJECT_ID,
                 storageBucket: process.env.STORAGE_BUCKET,
                 messagingSenderId: process.env.MESSAGING_SENDER_ID,
-                appId: process.env.APP_ID
+                appId: process.env.APP_ID,
+                measurementId: process.env.MEASUREMENT_ID
             }
         }
     }
