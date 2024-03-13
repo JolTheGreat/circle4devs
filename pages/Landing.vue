bdfa9634f70a70f61b446b668d56fafaa081869f
jolthegreat <katoshin0805@gmail.com>
2024-03-13 22:05:06 +0900

    Meta tags

--- pages/Landing.vue
+++ pages/Landing.vue
@@ -2,6 +2,8 @@
   <Head>
     <Title>Circle4Devsとは？</Title>
     <Meta name="description" content="開発したアプリを発信しよう！"/>
+    <Meta name="keywords" content="Circle4Devs, アプリ, 開発, プラットフォーム"/>
+    <Meta name="author" content="Circle4Devs"/>
     <Meta property="og:title" content="Circle4Devsとは？"/>
     <Meta property="og:description" content="開発したアプリを発信しよう！"></Meta>
     <Meta property="og:url" content="https://circle4devs.netlify.app/"/>
@@ -13,6 +15,7 @@
     <Meta name="twitter:description" content="開発したアプリを発信しよう！"/>
     <Meta name="twitter:url" content="https://circle4devs.netlify.app/"/>
     <Meta name="twitter:domain" content="Circle4Devs"/>
+    <Meta name="twitter:image" content="https://firebasestorage.googleapis.com/v0/b/circle-404609.appspot.com/o/logo.png?alt=media&token=2baccc57-95f9-4c60-b80c-c74d8fb51cf9"/>
 
     <Meta name="google-site-verification" content="ss-wDpzNen2Gl_UZVF9Y4x_Bg2APqP5TwLZQp7y9Obk" />
   </Head>
