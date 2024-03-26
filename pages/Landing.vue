e761fd298ae0f6765a498cbe064883c0d6b2943b
jolthegreat <katoshin0805@gmail.com>
2024-03-26 09:40:09 +0900

    Landing

--- pages/Landing.vue
+++ pages/Landing.vue
@@ -10,16 +10,18 @@
     <Meta property="og:url" content="https://circle4devs.netlify.app/"/>
     <Meta property="og:type" content="website"/>
     <Meta property="og:site_name" content="Circle4Devs"/>
-    <Meta property="og:image" content="https://firebasestorage.googleapis.com/v0/b/circle-404609.appspot.com/o/logo.png?alt=media&token=2baccc57-95f9-4c60-b80c-c74d8fb51cf9"/>
-`````    <Meta name="twitter:card" content="summary_large_image"/>`````
+    <Meta property="og:image"
+          content="https://firebasestorage.googleapis.com/v0/b/circle-404609.appspot.com/o/logo.png?alt=media&token=2baccc57-95f9-4c60-b80c-c74d8fb51cf9"/>
+    <Meta name="twitter:card" content="summary_large_image"/>
     <Meta name="twitter:site" content="@Circle4Devs"/>
     <Meta name="twitter:title" content="Circle4Devsとは？"/>
     <Meta name="twitter:description" content="開発したアプリを発信しよう！"/>
     <Meta name="twitter:url" content="https://circle4devs.netlify.app/"/>
     <Meta name="twitter:domain" content="Circle4Devs"/>
-    <Meta name="twitter:image" content="https://firebasestorage.googleapis.com/v0/b/circle-404609.appspot.com/o/logo.png?alt=media&token=2baccc57-95f9-4c60-b80c-c74d8fb51cf9"/>
+    <Meta name="twitter:image"
+          content="https://firebasestorage.googleapis.com/v0/b/circle-404609.appspot.com/o/logo.png?alt=media&token=2baccc57-95f9-4c60-b80c-c74d8fb51cf9"/>
 
-    <Meta name="google-site-verification" content="ss-wDpzNen2Gl_UZVF9Y4x_Bg2APqP5TwLZQp7y9Obk" />
+    <Meta name="google-site-verification" content="ss-wDpzNen2Gl_UZVF9Y4x_Bg2APqP5TwLZQp7y9Obk"/>
   </Head>
   <div id="landing">
     <div class="title-page">
